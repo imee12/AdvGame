ca6092b64762194cee063e5d2128979d2df074a1
Imee Cuison <imeetwelve@gmail.com>
2015-01-15 16:40:42 -0500

    They're dressed.

---
+++ index.js
@@ -0,0 +1,54 @@
+
+console.log("Welcome!")
+
+var player=("chick", "dude")
+
+var player=prompt("Are you a chick or a dude?")
+
+if(player==="chick"){
+  console.log("Let's play, Shady Lady!");
+} else if (player==="dude") {
+  console.log("Let's go, Bro!");
+};
+
+if(player==="chick") {
+  console.log("You are going out for a night on town with your girls.");
+}
+
+if(player==="dude") {
+console.log("You are going out for a night on the town with your boys.");
+}
+
+
+console.log("You are getting dressed.")
+
+if(player=="chick"){
+  var clothes=prompt("What are you going to wear? A: Flip flops and cutoff jean shorts. B:Skirt and heels.")
+}
+
+var clothes=("A","B")
+
+if(clothes==="A") {
+  console.log("You'll be the best dressed girl in the barn. Way to go!");
+} else if (clothes==="B") {
+  console.log("Too much. You look desperate!");
+}
+
+
+if(player=="dude"){
+    var clothes=prompt("What are you going to wear? A: Flip flops and shorts. B: Button down shirt and jeans.")
+  }
+
+  var clothes=("A","B")
+
+  if(clothes==="A") {
+    console.log("Some girls like the homeless look... I guess.");
+  } else if (clothes==="B") {
+    console.log("Perfect. Players don't look like they try to hard.")
+}
+
+
+/*
+if(player==="dude"){
+  prompt("What uuup?")
+}*/
