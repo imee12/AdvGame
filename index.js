6957ce2fac35b6a785af7565d5ce44c4bbadac7d
Imee Cuison <imeetwelve@gmail.com>
2015-01-15 22:39:01 -0500

    tryingscore

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-
-console.log("Welcome!")
+/*
+console.log("Welcome!")*/
 
 var player=("chick", "dude")
-
+/*
 var player=prompt("Are you a chick or a dude?")
 
 if(player==="chick"){
@@ -20,7 +20,7 @@ console.log("You are going out for a night on the town with your boys.");
 }
 
 /*Put in playa (hard) or novice (easy)?*/
-
+/*
 console.log("We are going to keep score now. So get ready!")
 
 
@@ -29,33 +29,47 @@ confirm("Are you ready?");
 
 console.log("You are getting dressed.")
 
-if(player=="chick"){
+if(player==="chick"){
+
+*/
   var clothes=prompt("What are you going to wear? A: Flip flops and cutoff jean shorts. B:Skirt and heels.")
+
+
+
+var scoreA=0;
+var scoreB=1;
+
+if(player==="chick"){
+if(clothes==="A") {
+  console.log("You'll be the best dressed girl in the barn. Way to go! SCORE=0");
+} else if(clothes==="B") {
+  console.log("Perfect! Players don't look like they try to hard. SCORE=1");
+}
 }
 
-var clothes=("A","B");
-
-if(clothes="A") {
-  console.log("You'll be the best dressed girl in the barn. Way to go!");
-} else if (clothes="B") {
-  console.log("Perfect! Players don't look like they try to hard.");
-}
 
 
-if(player==="dude"){
-    var clothes=prompt("What are you going to wear? A: Flip flops and shorts. B: Button down shirt and jeans.")
+var score =function(){
+  var scoreA=0;
+  var scoreB=1;
+  if(0){
+    return(0);
+  }else (1);{
+    return(1)
   }
-
-  var clothes=("A","B")
-
-  if(clothes="A") {
-    console.log("Some girls like the homeless look... I guess.");
-  } else if (clothes="B") {
-    console.log("Perfect. Players don't look like they try to hard.")
 }
-
 
 /*
 if(player==="dude"){
-  prompt("What uuup?")
+
+  var clothes=("A","B")
+    var clothes=prompt("What are you going to wear? A: Flip flops and shorts. B: Button down shirt and jeans.")
+  }
+
+if(player==="dude"){
+  if(clothes==="A") {
+    console.log("Some girls like the homeless look... I guess.");
+  } else if (clothes==="B") {
+    console.log("Perfect. Players don't look like they try to hard.")
+}
 }*/
