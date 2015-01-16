2f490d97fb913b3d3c18b856934ceb6483349f8a
Imee Cuison <imeetwelve@gmail.com>
2015-01-15 19:03:23 -0500

    Dressed appropriately

--- index.js
+++ index.js
@@ -26,24 +26,24 @@ if(player=="chick"){
   var clothes=prompt("What are you going to wear? A: Flip flops and cutoff jean shorts. B:Skirt and heels.")
 }
 
-var clothes=("A","B")
+var clothes=("A","B");
 
-if(clothes==="A") {
+if(clothes="A") {
   console.log("You'll be the best dressed girl in the barn. Way to go!");
-} else if (clothes==="B") {
-  console.log("Too much. You look desperate!");
+} else if (clothes="B") {
+  console.log("Perfect! Players don't look like they try to hard.");
 }
 
 
-if(player=="dude"){
+if(player==="dude"){
     var clothes=prompt("What are you going to wear? A: Flip flops and shorts. B: Button down shirt and jeans.")
   }
 
   var clothes=("A","B")
 
-  if(clothes==="A") {
+  if(clothes="A") {
     console.log("Some girls like the homeless look... I guess.");
-  } else if (clothes==="B") {
+  } else if (clothes="B") {
     console.log("Perfect. Players don't look like they try to hard.")
 }
 
